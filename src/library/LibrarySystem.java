4e335ffc0fcc0650f4dce7ad3dc34f0fc68d1a6a
mariamismaiel <76557785+mariamismaiel@users.noreply.github.com>
2020-12-28 15:45:36 +0200

    Update LibrarySystem.java

--- src/library/LibrarySystem.java
+++ src/library/LibrarySystem.java
@@ -6,7 +6,7 @@
 package Library;
 
 import java.util.Scanner;
-
+import java.time.*;
 /**
  *
  * @author yosef
@@ -19,52 +19,73 @@ public class LibrarySystem {
     public static void main(String[] args) {
        
        Librarians admin[]=new Librarians[5];
+    
+    
        
        //initialization of librarians
        admin[0]=new Librarians(1,"admin1@admn.com","1111","Librarians","Ahmed","Hassan",
-               "ABC","201078496455",false,false,null,null,null); 
+               "ABC","201078496455",false,false,null,null,LocalDate.of(2030, 1, 2)); 
        
        admin[1]=new Librarians(2, "admin2@admn.com", "2222", "librarian", "Ali", "Mohamed",
-               "XYZ", "201123947957", false,false,null,null,null);
+               "XYZ", "201123947957", false,false,null,null,LocalDate.of(2020, 1, 2));
        
        admin[2]=new Librarians(3, "admin3@admn.com", "3333", "librarian", "Mohamed", "Fathy",
-               "LMN", "201038774654", false,false,null,null,null);
+               "LMN", "201038774654", false,false,null,null,LocalDate.of(2020, 1, 2));
        
        admin[3]=new Librarians(4, "admin4@admn.com", "4444", "librarian", "Sara", "Ahmed",
-               "DFG", "201118476563", false,false,null,null,null);
+               "DFG", "201118476563", false,false,null,null,LocalDate.of(2020, 1, 2));
        
        admin[4]=new Librarians(5, "admin5@admn.com", "5555", "librarian", "Soha", "Akram",
-               "XXX", "201038365454", false,false,null,null,null);
+               "XXX", "201038365454", false,false,null,null,LocalDate.of(2030, 1, 2));
        
        
-        for(int i=5;i<100;i++){
-           reader[i]=new Readers(0,"empty","empty","empty","empty","empty","empty","empty",false,false,"empty",LocalDate.of(2030 , 1 , 1),LocalDate.of(2030,1,1));
-       }
+       Readers reader[]=new Readers[100];
        
        //initialization of readers
        reader[0]=new Readers(11, "karim11@reader.com", "karim11", "reader", "Karim", "Hossam",
-               "ZZZ", "01091751184", false,false,null,null,null);
+               "ZZZ", "01091751184", false,false,null,null,LocalDate.of(2020, 1, 2));
        
        reader[1]=new Readers(22, "mona22@reader.com", "mona22", "reader", "Mona", "Hamdy",
-               "KLM", "01001234567", true,false,null,null,null);
+               "KLM", "01001234567", true,false,null,null,LocalDate.of(2030, 1, 2));
        
        reader[2]=new Readers(33, "khaled33@reader.com", "khaled33", "reader", "Khaled", "Hassan",
-               "YYY", "01234567896", true,false,null,null,null);
+               "YYY", "01234567896", true,false,null,null,LocalDate.of(2030, 1, 2));
        
        reader[3]=new Readers(44, "amr44@reader.com", "amr44", "reader", "Amr", "Ahmed", 
-               "FGH", "01167854378", false,false,null,null,null);
+               "FGH", "01167854378", false,false,null,null,LocalDate.of(2030, 1, 2));
        
        reader[4]=new Readers(55, "omar55@reader.com", "omar55", "reader", "Omar", "Ashraf",
-               "FFF", "014456565662", false,false,null,null,null);
+               "FFF", "014456565662", false,false,null,null,LocalDate.of(2020, 1, 2));
+       
+     
        
        
         Books book[]=new Books[200];
-        
         //initialization of books
-        //book[0]=new Books("Harry Potter", 20, "fantasy"); 
+        book[0]=new Books("x", 1, "x", new String[]{}); 
+        book[1]=new Books("y", 1, "x", new String[]{}); 
+        book[2]=new Books("z", 1, "x", new String[]{}); 
+        book[3]=new Books("a", 1, "x", new String[]{}); 
+        book[4]=new Books("b", 1, "x", new String[]{}); 
         
+        //initialization of readers by default values
+         for(int i=5;i<100;i++)
+       {
+         reader[i]=new Readers(0, "empty", "empty", "empty", "empty", "empty",
+               "empty", "empty", false,false,"empty",LocalDate.of(2030, 1, 1),LocalDate.of(2030, 1, 1));
         
+         
+       }
+         //intialization of books by default values
+         for(int i=5;i<200;i++)
+         {
+             book[i]=new Books("empty",0,"empty",new String[] {});
+             
+         }
+       
+         
         
+       
         while(true)
         {
            String Email,password;

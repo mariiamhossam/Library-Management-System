0249d03b2677a16e88813b625c04ed308402ee34
esraa176 <72940480+esraa176@users.noreply.github.com>
2021-02-06 05:17:51 +0200

    Update Books.java

--- src/library/Books.java
+++ src/library/Books.java
@@ -10,21 +10,21 @@ package Library;
  * @author yosef
  */
 public class Books {
-     public String Name;
+
+    public String Name;
     public int quantity;
     public String type;
     public int orderList[] = new int[200];
     public int counter = 0;  //Counter for number of users in orderList 
 
-    public Books(){
-        
+    public Books() {
+
     }
-    
-    public Books(String Name,int quantity,String type,int orderlist[])
-    {
-        this.Name=Name;
-        this.quantity=quantity;
-        this.type=type;
-        this.orderList=orderlist;
+
+    public Books(String Name, int quantity, String type, int orderlist[]) {
+        this.Name = Name;
+        this.quantity = quantity;
+        this.type = type;
+        this.orderList = orderlist;
     }
 }

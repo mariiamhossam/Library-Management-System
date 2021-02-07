383c3d6ec9fa1301e28ee5fffefa7b7345cec9f2
RawanSadek <76558250+RawanSadek@users.noreply.github.com>
2021-02-07 17:56:01 +0200

    Update Users.java

--- src/library/Users.java
+++ src/library/Users.java
@@ -29,15 +29,43 @@ public abstract class Users {
     public LocalDate rentDate;
     public LocalDate Deadline_Date;
 
-    public abstract int searchForBook(String bookName, int index, Books[] book);
+    public int searchForBook(String bookName, int index, Books[] book)
+    {
+        int bookIndex = -1;
+        for (int i = 0; i < 200; i++) {
+            if (bookName.equals(book[i].Name)) {
+                bookIndex = i;
+                break;
+            }
+        }
+        return bookIndex;
+    }
 
     public abstract int searchForUser(int memberID, Readers[] reader, Librarians[] admin);
 
-    public abstract void rent(String name, Books[] book, int bookIndex);
+    public void rent(String name, Books[] book, int bookIndex)
+    {
+        isRent = true;
+        Book_name = name;
+        rentDate = LocalDate.now();
+        LocalDate deadline = rentDate.plusDays(7);
+        Deadline_Date = deadline;
+        book[bookIndex].quantity--;
+        System.out.println("you have successfully rented the book! you must return it before " + Deadline_Date);
+        System.out.println("_______________________________________");
+    }
 
     public abstract Boolean Check_isBlocked();
 
     public abstract boolean Check_isRentedBefore();
 
-    public abstract void Return_book(Books b[], int book_indx, Readers[] r, int user_indx);
+    public void Return_book(Books b[], int book_indx, Readers[] r, int user_indx)
+    {
+        b[book_indx].quantity++;
+        r[user_indx].isRent = false;
+        r[user_indx].rentDate = LocalDate.of(2030, 1, 2);
+        r[user_indx].Deadline_Date = LocalDate.of(2030, 1, 2);
+        System.out.println("The book has been successfully returned");
+        System.out.println("_______________________________________");
+    }
 }

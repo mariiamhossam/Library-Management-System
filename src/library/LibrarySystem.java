ce7dca0e79e7569e95d225d282f52ab67ae2c4a1
RawanSadek <76558250+RawanSadek@users.noreply.github.com>
2021-03-16 23:51:11 +0200

    Update LibrarySystem.java

--- src/library/LibrarySystem.java
+++ src/library/LibrarySystem.java
@@ -5,8 +5,11 @@
  */
 package Library;
 
+import Library.users.Librarians;
+import Library.users.Readers;
 import java.util.Scanner;
 import java.time.*;
+
 
 /**
  *
@@ -22,38 +25,38 @@ public class LibrarySystem {
         Librarians admin[] = new Librarians[5];
 
         //initialization of librarians
-         admin[0] = new Librarians(1, "admin1@admn.com", "1111", "Librarians", "Ahmed", "Hassan",
-                "ABC", "201078496455", false, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2030, 1, 2));
+        admin[0] = new Librarians(1, "admin1@admn.com", "1111", "Librarians", "Ahmed", "Hassan",
+                "ABC", "201078496455", false, false, "", null, LocalDate.of(2030, 1, 2));           //Book name is "" instead of null
 
         admin[1] = new Librarians(2, "admin2@admn.com", "2222", "librarian", "Ali", "Mohamed",
-                "XYZ", "201123947957", false, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2020, 1, 2));
+                "XYZ", "201123947957", false, false, "", null, LocalDate.of(2020, 1, 2));
 
         admin[2] = new Librarians(3, "admin3@admn.com", "3333", "librarian", "Mohamed", "Fathy",
-                "LMN", "201038774654", false, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2020, 1, 2));
+                "LMN", "201038774654", false, false, "", null, LocalDate.of(2020, 1, 2));
 
         admin[3] = new Librarians(4, "admin4@admn.com", "4444", "librarian", "Sara", "Ahmed",
-                "DFG", "201118476563", false, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2020, 1, 2));
+                "DFG", "201118476563", false, false, "", null, LocalDate.of(2020, 1, 2));
 
         admin[4] = new Librarians(5, "admin5@admn.com", "5555", "librarian", "Soha", "Akram",
-                "XXX", "201038365454", false, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2030, 1, 2));
+                "XXX", "201038365454", false, false, "", null, LocalDate.of(2030, 1, 2));
 
         Readers reader[] = new Readers[100];
 
         //initialization of readers
         reader[0] = new Readers(11, "karim11@reader.com", "karim11", "reader", "Karim", "Hossam",
-                "ZZZ", "01091751184", false, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2020, 1, 2));
+                "ZZZ", "01091751184", false, false, "", null, LocalDate.of(2020, 1, 2));            //Book name is "" instead of null
 
         reader[1] = new Readers(22, "mona22@reader.com", "mona22", "reader", "Mona", "Hamdy",
-                "KLM", "01001234567", true, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2030, 1, 2));
+                "KLM", "01001234567", true, false, "", null, LocalDate.of(2030, 1, 2));
 
         reader[2] = new Readers(33, "khaled33@reader.com", "khaled33", "reader", "Khaled", "Hassan",
-                "YYY", "01234567896", true, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2030, 1, 2));
+                "YYY", "01234567896", true, false, "", null, LocalDate.of(2030, 1, 2));
 
         reader[3] = new Readers(44, "amr44@reader.com", "amr44", "reader", "Amr", "Ahmed",
-                "FGH", "01167854378", false, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2030, 1, 2));
+                "FGH", "01167854378", false, false, "", null, LocalDate.of(2030, 1, 2));
 
         reader[4] = new Readers(55, "omar55@reader.com", "omar55", "reader", "Omar", "Ashraf",
-                "FFF", "014456565662", false, false, "empty", LocalDate.of(2030, 1, 1), LocalDate.of(2020, 1, 2));
+                "FFF", "014456565662", false, false, "", null, LocalDate.of(2020, 1, 2));
 
         Books book[] = new Books[200];
         //initialization of books
@@ -92,7 +95,7 @@ public class LibrarySystem {
             boolean isFound = false;
             for (int i = 0; i < 5; i++) {
 
-                if (Email.equals(admin[i].Email) && password.equals(admin[i].password)) {
+                if (Email.equals(admin[i].Email) && password.equals(admin[i].getPassword())) {
                     isFound = true;
                     userIndex = i;
                     lib.Admin_page(userIndex, reader, admin, book);
@@ -102,13 +105,13 @@ public class LibrarySystem {
 
             if (isFound == false) {
                 for (int i = 0; i < 100; i++) {
-                    if (Email.equals(reader[i].Email) && password.equals(reader[i].password)) {
+                    if (Email.equals(reader[i].Email) && password.equals(reader[i].getPassword())) {
 
                         userIndex = i;
                         r.Reader_page(userIndex, reader, admin, book);
-                        isFound = true;
+                        isFound=true;
                         break;
-                    }
+                    } 
 
                 }
             }
